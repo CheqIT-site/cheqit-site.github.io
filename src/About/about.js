90409d8981cc98e112d3924eac94aae0b4d42dc3
amtulkhan <amtulmaswaraahmed@gmail.com>
2021-01-23 12:35:35 +0530

    i suck :(

--- src/About/about.js
+++ src/About/about.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import "./about.css";
+import HomeNavbar from "../HomeNavbar/HomeNavbar";
 
-
-export default function About(){
-    return(
-        <div></div>
-    )
+export default function About() {
+  return (
+    <div className="about-main">
+        
+    </div>
+  );
 }
