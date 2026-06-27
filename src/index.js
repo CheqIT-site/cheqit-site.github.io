a8c778efd6f23fdbaa4c09f1d2ca5f612bb86515
Raj Kumar <rajemailaddress007@gmail.com>
2026-06-27 18:19:53 -0400

    Add GitHub Pages SPA fallback

--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("p");
+
+if (redirectPath) {
+  const normalizedPath = redirectPath.startsWith("/") ? redirectPath : `/${redirectPath}`;
+  window.history.replaceState(null, "", `/Vector/#${normalizedPath}`);
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
